f635d73062c590f1b4ac7be29f11bb70dc5cbd8d
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-07-12 21:34:14 +0600

    first

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 const numbers =5555;
 const singer = {name: 'dr. Mahafuz', job:'singer'}
-const singer2 = {name:'Eva Rahman', job:'Singar'}
+const singer2 = {name:'Eva Rahman', job:'Singer'}
+
+const singerStyle ={
+  color: 'purple',
+  backgroundColor: 'white'
+}
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      <h1>name: {2222 + numbers}</h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <p>name:{singer.name} job {singer.job}</p>
-        <p>name:{singer.name} job{singer.job}</p>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite  React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New Component</h5>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+    </div>
+  )
+}
+
+function Person(){
+  return (
+    <div className='person'>
+      <h1>Shakib Al Hasan</h1>
+      <p>profession: Cricketer</p>
+    </div>
+  )
+}
+
+function Friend(){
+  return(
+    <div className='container'>
+      <h3>name: Ajay Devgan</h3>
+      <p>job: maramari</p>
+    </div>
   )
 }
 
